86f1c77d66ea3fceece7a980f315669b3810994e
Tuhacrt <a067800497@gmail.com>
2023-09-01 15:53:01 +0800

    chore: turn off eslint rules

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,8 +2,10 @@ import { describe, expect, it } from 'vitest'
 
 import { onePlusTwo } from '../src'
 
-describe('should', () => {
-  it('exported', () => {
+describe('onePlusTwo()', () => {
+  it(`Given: null
+      When: onePlusTwo
+      Then: return 3`, () => {
     const example = onePlusTwo
     const expected = 3
 
