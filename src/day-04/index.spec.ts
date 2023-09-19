80743ff79eb2600973cc6693db090bcf4f440627
Tuhacrt <a067800497@gmail.com>
2023-09-19 22:00:24 +0800

    fix: 🐛 remove unused console.log

--- src/day-04/index.spec.ts
+++ src/day-04/index.spec.ts
@@ -1,39 +1,39 @@
-// import { describe, expect, test } from 'bun:test';
+import { describe, expect, test } from 'bun:test';
 
-// import { fibDec, fibImp, pascalDec, pascalImp } from './index';
+import { fibDec, fibImp, pascalDec, pascalImp } from './index';
 
-// describe('Two fib are both valid', () => {
-//   test('fibImp(num)', () => {
-//     const num = 10;
-//     const received = fibImp(num);
-//     const expected = 55;
+describe('Two fib are both valid', () => {
+  test('fibImp(num)', () => {
+    const num = 10;
+    const received = fibImp(num);
+    const expected = 55;
 
-//     expect(received).toEqual(expected);
-//   });
+    expect(received).toEqual(expected);
+  });
 
-//   test('fibDec(num)', () => {
-//     const num = 10;
-//     const received = fibDec(num);
-//     const expected = 55;
+  test('fibDec(num)', () => {
+    const num = 10;
+    const received = fibDec(num);
+    const expected = 55;
 
-//     expect(received).toEqual(expected);
-//   });
-// });
+    expect(received).toEqual(expected);
+  });
+});
 
-// describe('Two pascal are both valid', () => {
-//   test('pascalImp(num)', () => {
-//     const num = 5;
-//     const received = pascalImp(num);
-//     const expected = [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]];
+describe('Two pascal are both valid', () => {
+  test('pascalImp(num)', () => {
+    const num = 5;
+    const received = pascalImp(num);
+    const expected = [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]];
 
-//     expect(received).toEqual(expected);
-//   });
+    expect(received).toEqual(expected);
+  });
 
-//   test('pascalDec(num)', () => {
-//     const num = 5;
-//     const received = pascalDec(num);
-//     const expected = [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]];
+  test('pascalDec(num)', () => {
+    const num = 5;
+    const received = pascalDec(num);
+    const expected = [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]];
 
-//     expect(received).toEqual(expected);
-//   });
-// });
+    expect(received).toEqual(expected);
+  });
+});
