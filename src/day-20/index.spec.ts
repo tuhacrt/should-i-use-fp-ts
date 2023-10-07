dd49b7969756c64e1ca88b957f76f1dff87f3c70
Tuhacrt <a067800497@gmail.com>
2023-10-08 00:05:48 +0800

    feat: 🎸 add day-21-apply

--- src/day-20/index.spec.ts
+++ src/day-20/index.spec.ts
@@ -1,30 +1,30 @@
-/* eslint-disable ts/ban-ts-comment */
-import { describe, expect, it } from 'bun:test';
-import * as E from 'fp-ts/Either';
+// /* eslint-disable ts/ban-ts-comment */
+// import { describe, expect, it } from 'bun:test';
+// import * as E from 'fp-ts/Either';
 
-export const normalParse = (...args: Parameters<typeof JSON.parse>) => {
-  try {
-    return JSON.parse(...args);
-  } catch (error) {
-    return error;
-  }
-};
+// export const normalParse = (...args: Parameters<typeof JSON.parse>) => {
+//   try {
+//     return JSON.parse(...args);
+//   } catch (error) {
+//     return error;
+//   }
+// };
 
-const parse = (...args: Parameters<typeof JSON.parse>) => E.tryCatch(
-  () => JSON.parse(...args),
-  e => e instanceof Error ? e : new Error('Unexpected error'),
-);
+// const parse = (...args: Parameters<typeof JSON.parse>) => E.tryCatch(
+//   () => JSON.parse(...args),
+//   e => e instanceof Error ? e : new Error('Unexpected error'),
+// );
 
-describe('Either is correctly implement', () => {
-  it(`
-  Given: user
-  When: parse(user)
-  Then: E.isLeft()`, () => {
-    const user = { username: 'test', email: '', password: '' };
-    // @ts-expect-error
-    const received = parse(user);
-    const expected = true;
+// describe('Either is correctly implement', () => {
+//   it(`
+//   Given: user
+//   When: parse(user)
+//   Then: E.isLeft()`, () => {
+//     const user = { username: 'test', email: '', password: '' };
+//     // @ts-expect-error
+//     const received = parse(user);
+//     const expected = true;
 
-    expect(E.isLeft(received)).toEqual(expected);
-  });
-});
+//     expect(E.isLeft(received)).toEqual(expected);
+//   });
+// });
